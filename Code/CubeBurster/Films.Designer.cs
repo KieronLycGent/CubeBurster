d754479f2ba98d04a971708fa91a31a715f36d7e
Kieron Parmentier <boerestink1@gmail.com>
2021-06-03 12:25:35 +0200

    frmFilms geüpdate

--- Code/CubeBurster/Films.Designer.cs
+++ Code/CubeBurster/Films.Designer.cs
@@ -31,8 +31,11 @@ namespace CubeBurster
         {
             this.tcFilms = new System.Windows.Forms.TabControl();
             this.tpFilms = new System.Windows.Forms.TabPage();
+            this.dgvFilms = new System.Windows.Forms.DataGridView();
             this.tpAddFilms = new System.Windows.Forms.TabPage();
             this.tcFilms.SuspendLayout();
+            this.tpFilms.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilms)).BeginInit();
             this.SuspendLayout();
             // 
             // tcFilms
@@ -40,40 +43,62 @@ namespace CubeBurster
             this.tcFilms.Controls.Add(this.tpFilms);
             this.tcFilms.Controls.Add(this.tpAddFilms);
             this.tcFilms.Location = new System.Drawing.Point(0, 0);
+            this.tcFilms.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tcFilms.Name = "tcFilms";
             this.tcFilms.SelectedIndex = 0;
-            this.tcFilms.Size = new System.Drawing.Size(801, 450);
+            this.tcFilms.Size = new System.Drawing.Size(977, 508);
             this.tcFilms.TabIndex = 0;
             // 
             // tpFilms
             // 
-            this.tpFilms.Location = new System.Drawing.Point(4, 25);
+            this.tpFilms.Controls.Add(this.dgvFilms);
+            this.tpFilms.Location = new System.Drawing.Point(4, 22);
+            this.tpFilms.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tpFilms.Name = "tpFilms";
-            this.tpFilms.Padding = new System.Windows.Forms.Padding(3);
-            this.tpFilms.Size = new System.Drawing.Size(793, 421);
+            this.tpFilms.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tpFilms.Size = new System.Drawing.Size(969, 482);
             this.tpFilms.TabIndex = 0;
             this.tpFilms.Text = "Films";
             this.tpFilms.UseVisualStyleBackColor = true;
             // 
+            // dgvFilms
+            // 
+            this.dgvFilms.AllowUserToAddRows = false;
+            this.dgvFilms.AllowUserToDeleteRows = false;
+            this.dgvFilms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilms.Location = new System.Drawing.Point(0, 0);
+            this.dgvFilms.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvFilms.Name = "dgvFilms";
+            this.dgvFilms.ReadOnly = true;
+            this.dgvFilms.RowHeadersWidth = 51;
+            this.dgvFilms.RowTemplate.Height = 24;
+            this.dgvFilms.Size = new System.Drawing.Size(969, 482);
+            this.dgvFilms.TabIndex = 1;
+            // 
             // tpAddFilms
             // 
-            this.tpAddFilms.Location = new System.Drawing.Point(4, 25);
+            this.tpAddFilms.Location = new System.Drawing.Point(4, 22);
+            this.tpAddFilms.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tpAddFilms.Name = "tpAddFilms";
-            this.tpAddFilms.Padding = new System.Windows.Forms.Padding(3);
-            this.tpAddFilms.Size = new System.Drawing.Size(793, 421);
+            this.tpAddFilms.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tpAddFilms.Size = new System.Drawing.Size(593, 340);
             this.tpAddFilms.TabIndex = 1;
             this.tpAddFilms.Text = "Films Toevoegen";
             this.tpAddFilms.UseVisualStyleBackColor = true;
             // 
             // frmFilms
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(977, 506);
             this.Controls.Add(this.tcFilms);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmFilms";
             this.Text = "Films";
+            this.Load += new System.EventHandler(this.frmFilms_Load);
             this.tcFilms.ResumeLayout(false);
+            this.tpFilms.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilms)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +108,6 @@ namespace CubeBurster
         private System.Windows.Forms.TabControl tcFilms;
         private System.Windows.Forms.TabPage tpFilms;
         private System.Windows.Forms.TabPage tpAddFilms;
+        private System.Windows.Forms.DataGridView dgvFilms;
     }
 }
--- Code/CubeBurster/Films.cs
+++ Code/CubeBurster/Films.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+
 
 namespace CubeBurster
 {
@@ -15,6 +17,17 @@ namespace CubeBurster
         public frmFilms()
         {
             InitializeComponent();
+        }
+
+        private void frmFilms_Load(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= ../../cubeburster.accdb");
+            con.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT f.titel,f.jaaruitgave,f.regisseur,f.hoofdrol,g.genre,f.aantal,f.prijs,f.trailer FROM tblfilms f, tblgenres g WHERE f.genreID = g.genreID AND aantal > 0", con);
+            DataSet inhoud = new DataSet();
+            adapter.Fill(inhoud);
+            inhoud.DataSetName = "alleFilms";
+            dgvFilms.DataSource = inhoud.Tables[0];
         }
     }
 }
